317ff0e44dc0071d226ecbc002d03cd9f4c3fa80
deontay jefferson <djefferson46@gmail.com>
2021-11-08 09:18:11 -0800

    Fundamental concepts

--- dist/typescriptDemo.js
+++ dist/typescriptDemo.js
@@ -1,2 +1,22 @@
 var hello = "world"; //implicit
 var hello2 = "world"; //explicit
+//function
+var getFullname = function (name, lastName) {
+    return name + " " + lastName;
+};
+///
+var user = {
+    name: "tom",
+    age: 30
+};
+var user2 = {
+    name: "tom",
+    age: 30
+};
+var myCar = {
+    make: "Honda",
+    model: "Odd",
+    hunkHorn: function () {
+        return "honk";
+    }
+};
--- src/typescriptDemo.ts
+++ src/typescriptDemo.ts
@@ -1,4 +1,43 @@
 let hello = "world"; //implicit
 let hello2: string = "world"; //explicit
 
+//function
+const getFullname = (name: string, lastName: string) =>{
+    return `${name} ${lastName}`;
+}
+
+///
+
+const user:{name: string, age: number} = { //this sets what type of data in obj
+    name: "tom",
+    age: 30
+};
+
+//interfaces => creates objs with props
+
+interface IUser{
+    name: "tom",
+    age?: 30 //? makes it optional
+}
+
+const user2: IUser = { //=> replaces ...user:{name: string, age: number}...
+    name: "tom",
+    age: 30
+};
+
+//interfaces with functions
+
+interface ICar{
+    make: string,
+    model: string,
+    hunkHorn(): string;
+}
+
+const myCar:ICar = {
+    make: "Honda",
+    model: "Odd",
+    hunkHorn (){ //note no arrow
+        return "honk";
+    },
+}
 
